4cef6b0542a6bdf8883151b3838cf762328a934e
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-11-04 11:08:09 -0800

    Add local image paths to scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ const {PI,sin,cos} = Math;
 
 const segments = 6; //Edit # of mirrors
 
-const offset = {x: 300, y: 50}; //Changes what part of image is shown --> Maybe use scroll bar control to edit this in app?
+const offset = {x: 200, y: 50}; //Changes what part of image is shown --> Maybe use scroll bar control to edit this in app?
 
 ctx.beginPath();
 ctx.arc(370, 370, 350, 0, 360);
@@ -62,11 +62,17 @@ function animate() {
 }
 
 const img = new Image();
-img.src = "./IMG/sample2.jpg";
+// img.src = "https://media.istockphoto.com/vectors/modern-trendy-abstract-shapes-in-pastel-colors-scandinavian-clean-vector-id1190577091?b=1&k=6&m=1190577091&s=612x612&w=0&h=tTQpM21MUcoH0maJlKYZY60Ov7BZH9ksXyorRU5XJWM="; //Example img
+
+// img.src = "https://i.pinimg.com/564x/f3/52/8c/f3528c650c4b2a162bb853c040bbbba5.jpg"; //Example img 2
+
+img.src = document.getElementById('sample-1'); 
 
 // img.onload = function() {
   const pattern = ctx.createPattern(img, 'repeat');
 // };
+
+// document.getElementById('body').appendChild(img); 
 
 animate();
 
