7069039e6057b63c9a9cb4de56ed0a861cd65c67
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-11-04 09:10:42 -0800

    Refactor kaleidoscope code

--- scripts.js
+++ scripts.js
@@ -15,17 +15,16 @@ ctx.clip();
 function animate() {
   const radius = w/2 + h/2;
   const mirrorAngle = (PI*2) / segments;
-  
-  const x = [ -1,
-  -1/*radius * sin(0)*/,
-  radius * sin(mirrorAngle),
-  radius * sin(mirrorAngle/2)
-];
-  const y = [ -1,
-  radius/*radius * cos(0)*/,
-  radius * cos(mirrorAngle),
-  radius * cos(mirrorAngle/2)
-];
+
+const x1 = -1;
+const x2 = -1;
+const x3 = radius * sin(mirrorAngle);
+const x4 = radius * sin(mirrorAngle/2);
+
+y1 = -1;
+y2 = radius;
+y3 = radius * cos(mirrorAngle);
+y4 = radius * cos(mirrorAngle/2);
   
 for(let i=0; i<segments; i++) {
       //Non-mirrored segment
@@ -33,10 +32,10 @@ for(let i=0; i<segments; i++) {
       ctx.rotate(i * mirrorAngle); //angle path depending on segment angle
       ctx.translate(offset.x, offset.y); 
       ctx.beginPath();
-      ctx.moveTo(x[0]-offset.x, y[0]-offset.y);
-      ctx.lineTo(x[1]-offset.x, y[1]-offset.y);
-      ctx.lineTo(x[2]-offset.x, y[2]-offset.y);
-      ctx.lineTo(x[0]-offset.x, y[0]-offset.y);
+      ctx.moveTo(x1-offset.x, y1-offset.y);
+      ctx.lineTo(x2-offset.x, y2-offset.y);
+      ctx.lineTo(x3-offset.x, y3-offset.y);
+      ctx.lineTo(x1-offset.x, y1-offset.y);
       ctx.fillStyle = pattern;
       ctx.fill();
       ctx.resetTransform();
@@ -47,10 +46,10 @@ for(let i=0; i<segments; i++) {
       ctx.scale(-1, 1);
       ctx.translate(offset.x, offset.y);
       ctx.beginPath();
-      ctx.moveTo(x[0]-offset.x, y[0]-offset.y);
-      ctx.lineTo(x[1]-offset.x, y[1]-offset.y);
-      ctx.lineTo(x[3]-offset.x, y[3]-offset.y);
-      ctx.lineTo(x[0]-offset.x, y[0]-offset.y);
+      ctx.moveTo(x1-offset.x, y1-offset.y);
+      ctx.lineTo(x2-offset.x, y2-offset.y);
+      ctx.lineTo(x4-offset.x, y4-offset.y);
+      ctx.lineTo(x1-offset.x, y1-offset.y);
       ctx.fillStyle = pattern;
       ctx.fill();
       ctx.resetTransform();
