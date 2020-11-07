2e2850cbab9da30c28d199f1290fbb0fe5f53e75
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-11-06 16:49:01 -0800

    Configure SketchAPI toolbar to make it expand on click

--- scripts.js
+++ scripts.js
@@ -13,7 +13,6 @@ function updateRender() {
   y4 = radius * cos(mirrorAngle/2);
     
   for(let i=0; i<segments; i++) {
-
     //Non-mirrored segment
     ctx.translate(w/2, h/2); //move to center of canvas
     ctx.rotate(i * mirrorAngle); //angle path depending on segment angle, multiplied by segment #
@@ -43,12 +42,11 @@ function updateRender() {
   }
 
   if(animating) {
-    offset.x = (offset.x + 0.75) % img.width;
-    offset.y = (offset.y + 0.25) % img.height; //Moves image before re-rendering/animation, CHANGE THIS TO CHANGE SPEED OF ANIMATION
+    offset.x = (offset.x + 0.75 + animationSpeed) % img.width;
+    offset.y = (offset.y + 0.25 + animationSpeed) % img.height; //Moves image before re-rendering/animation, CHANGE THIS TO CHANGE SPEED OF ANIMATION
     requestAnimationFrame(updateRender);
   }
 }
-
 
 function updateSegments() {
   segments = document.getElementById('mirror-slider').value;
@@ -57,10 +55,18 @@ function updateSegments() {
   }
 };
 
-function updateImgView() {
-  offsetViewVal = document.getElementById('view-slider').value;
-  if(!animating) {
+function updateKaleidoscopeView() { //only shows when animation paused
+  if(!animating){
+    viewVal = document.getElementById('view-slider').value;
+    offset.x = viewVal;
+    offset.y = viewVal;
     updateRender();
+  }
+};
+
+function updateSpeed() {
+  if(animating) {
+    animationSpeed = parseInt(document.getElementById('speed-slider').value);
   }
 };
 
@@ -73,17 +79,33 @@ const {PI,sin,cos} = Math;
 let animating = true;
 const animateButton = document.getElementById('animate-button').onclick = function() {
   animating = !animating;
+  if(animating) {
+    viewSlider.style.display = 'none';
+    speedSlider.style.display = 'inline';
+  } else {
+    viewSlider.style.display = 'inline';
+    speedSlider.style.display = 'none';
+  }
   updateRender();
 };
 
+//Kaleidoscope Toolbar
 let segments = document.getElementById('mirror-slider').value;
 const segmentSlider = document.getElementById('mirror-slider');
 segmentSlider.addEventListener('input', updateSegments);
 
-// let offsetViewVal = document.getElementById('view-slider').value;
-// const offsetSlider = document.getElementById('view-slider');
-const offset = {x: 100, y: 20};
-// offsetSlider.addEventListener('input', updateImgView);
+let viewVal = document.getElementById('view-slider').value;
+const viewSlider = document.getElementById('view-slider');
+viewSlider.style.display = 'none';
+const offset = {
+  x: 500, 
+  y: 200
+};
+viewSlider.addEventListener('input', updateKaleidoscopeView);
+
+let animationSpeed = document.getElementById('speed-slider').value;
+const speedSlider = document.getElementById('speed-slider');
+speedSlider.addEventListener('input', updateSpeed);
 
 ctx.beginPath();
 ctx.arc(370, 370, 350, 0, 360);
@@ -91,7 +113,6 @@ ctx.clip();
 
 const img = new Image();
 img.src = "./IMG/sample2.jpg"; 
-
 let pattern;
 img.onload = function() {
   pattern = ctx.createPattern(img, 'repeat');
